e98068d2d2a49d4746e33a42865833164d2853d9
Matthew Atkins <matkinsdesigns@gmail.com>
2018-05-01 21:38:27 -0400

    filled in circles

--- canvas.js
+++ canvas.js
@@ -61,6 +61,7 @@ function Circle (x, y, dx, dy, radius) {
     c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
     c.strokeStyle = 'black'
     c.stroke()
+    c.fill()
   }
 
   this.update = function () {
