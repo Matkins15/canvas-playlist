5d6a483e7ea6d64d52541773f18ce0d3c138b135
Matthew Atkins <matkinsdesigns@gmail.com>
2018-05-01 20:24:00 -0400

    drawing simple boxes within canvas

--- canvas.js
+++ canvas.js
@@ -1,5 +1,13 @@
 let canvas = document.querySelector('canvas')
-console.log(canvas)
 
 canvas.width = window.innerWidth
 canvas.height = window.innerHeight
+
+// c = contect
+let c = canvas.getContext('2d')
+
+c.fillRect(100, 100, 100, 100)
+c.fillRect(200, 200, 100, 100)
+c.fillRect(500, 500, 100, 100)
+
+console.log(canvas)
