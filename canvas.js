c54efacd4db49999a07639fa32e381bbb6a4d070
Matthew Atkins <matkinsdesigns@gmail.com>
2018-05-01 20:48:38 -0400

    generating multiple random shapes

--- canvas.js
+++ canvas.js
@@ -3,11 +3,45 @@ let canvas = document.querySelector('canvas')
 canvas.width = window.innerWidth
 canvas.height = window.innerHeight
 
-// c = contect
+// c = context
 let c = canvas.getContext('2d')
 
+c.fillStyle = 'purple'
 c.fillRect(100, 100, 100, 100)
-c.fillRect(200, 200, 100, 100)
-c.fillRect(500, 500, 100, 100)
+c.fillStyle = 'blue'
+c.fillRect(400, 200, 100, 100)
+c.fillStyle = 'orange'
+c.fillRect(600, 500, 100, 100)
 
 console.log(canvas)
+
+// lines
+
+// c.beginPath()
+// c.moveTo(50, 300)
+// c.lineTo(300, 100)
+// c.lineTo(400, 300)
+// c.strokeStyle = 'green'
+// c.stroke()
+
+for (let i = 0; i < 100; i++) {
+  let x = Math.random() * window.innerWidth
+  let y = Math.random() * window.innerHeight
+  c.beginPath()
+  c.moveTo(600, 300)
+  c.lineTo(x, y)
+  c.lineTo(x, y)
+  c.strokeStyle = 'green'
+  c.stroke()
+}
+
+// Arch / Circle
+
+for (var i = 0; i < 100; i++) {
+  let x = Math.random() * window.innerWidth
+  let y = Math.random() * window.innerHeight
+  c.beginPath()
+  c.arc(x, y, 30, 0, Math.PI * 2, false)
+  c.strokeStyle = 'black'
+  c.stroke()
+}
