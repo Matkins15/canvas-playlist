051d61b7a685153ebc9ff4b176d431c192cb84bf
Matthew Atkins <matkinsdesigns@gmail.com>
2018-05-01 23:53:33 -0400

    added init function to resize evnt listener

--- canvas.js
+++ canvas.js
@@ -1,8 +1,5 @@
 let canvas = document.querySelector('canvas')
 
-canvas.width = window.innerWidth
-canvas.height = window.innerHeight
-
 // c = context
 let c = canvas.getContext('2d')
 
@@ -47,6 +44,9 @@ console.log(canvas)
 // }
 
 // animation works by constantly refreshing the page while chaging the x and y values
+let maxRadius = 40
+let minRadius = 2
+
 let mouse = {
   x: undefined,
   y: undefined
@@ -57,19 +57,26 @@ window.addEventListener('mousemove', function (event) {
   mouse.y = event.y
 })
 
+window.addEventListener('resize', function () {
+  canvas.width = window.innerWidth
+  canvas.height = window.innerHeight
+
+  init()
+})
+
 function Circle (x, y, dx, dy, radius) {
   this.x = x
   this.y = y
   this.dx = dx
   this.dy = dy
   this.radius = radius
+  this.minRadius = minRadius
 
   this.draw = function () {
     c.beginPath()
     c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
-    c.strokeStyle = 'lightgrey'
+    c.fillStyle = 'lightgrey'
     c.fill()
-    c.stroke()
   }
 
   this.update = function () {
@@ -85,8 +92,10 @@ function Circle (x, y, dx, dy, radius) {
 
     // interactivity
     if (mouse.x - this.x < 50 && mouse.x - this.x > -50 && mouse.y - this.y < 50 && mouse.y - this.y > -50) {
-      this.radius += 1
-    } else if (this.radius > 2) {
+      if (this.radius < maxRadius) {
+        this.radius += 1
+      }
+    } else if (this.radius > this.minRadius) {
       this.radius -= 1
     }
 
@@ -94,14 +103,16 @@ function Circle (x, y, dx, dy, radius) {
   }
 }
 
-let circleArray = []
-for (let i = 0; i < 100; i++) {
-  let radius = 30
-  let x = Math.random() * (innerWidth - radius * 2) + radius
-  let y = Math.random() * (innerHeight - radius * 2) + radius
-  let dx = (Math.random() - 0.5) * 3
-  let dy = (Math.random() - 0.5) * 3
-  circleArray.push(new Circle(x, y, dx, dy, radius))
+function init () {
+  let circleArray = []
+  for (let i = 0; i < 300; i++) {
+    let radius = Math.random() * 3 + 1
+    let x = Math.random() * (innerWidth - radius * 2) + radius
+    let y = Math.random() * (innerHeight - radius * 2) + radius
+    let dx = (Math.random() - 0.5) * 2
+    let dy = (Math.random() - 0.5) * 2
+    circleArray.push(new Circle(x, y, dx, dy, radius))
+  }
 }
 
 const animate = () => {
