a92affd70d6580829d0b8ffbbed09581adaca358
Matthew Atkins <matkinsdesigns@gmail.com>
2018-05-02 00:33:43 -0400

    added text over canvas

--- canvas.js
+++ canvas.js
@@ -78,7 +78,7 @@ function Circle (x, y, dx, dy, radius) {
   this.draw = function () {
     c.beginPath()
     c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
-    c.fillStyle = 'lightgrey'
+    c.fillStyle = 'grey'
     c.fill()
   }
 
